b445863c36f5ac0e0ee946cf378d64756d020828
DickyChant <761337540@qq.com>
2020-02-22 11:44:08 +0800

    test

--- homework1.R
+++ homework1.R
@@ -1,16 +1,14 @@
 # Set up
-Args <- commandArgs()
-setwd(Args[6])
-cat("Current work dir:"," ")
-getwd()
-dataPath <- paste(getwd(),Args[7], sep = "/")
-cat("Data path: ",dataPath,"\n")
+setwd("~/codes/R")
+dataPath="rData"
 
 # Readin data
 WL <- read.table(paste(dataPath,"AQ_FSPMC_WanLiu.csv",sep="/"),skip = 3,header = TRUE, sep = ",")
 USE <- read.table(paste(dataPath,"AQ_FSPMC_USEmbassy.csv",sep="/"),skip = 3,header = TRUE, sep = ",")
 PRE <- read.table(paste(dataPath,"A_PRE.csv",sep = "/"),skip = 3,header = TRUE, sep = ",")
-TEMP <- read.table(paste(dataPath,"A_TEMP.csv",sep = "/"),skip = 3,header = TRUE, sep = ",")
+TEMP <- read.table(paste(dataPath,"A_TEMP.csv",sep = "/"),skip = 5,header = TRUE, sep = ",",
+                   col.names = c("Date","Time","Height","DegreeCelsius","Source","ErrFlag","Details"),
+                   colClasses = c("character","character",NA,NA,NA,NA,NA))
 RH <- read.table(paste(dataPath,"A_RH.csv",sep = "/"),skip = 3,header = TRUE, sep = ",")
 WIND <- read.table(paste(dataPath,"A_WIND.csv",sep = "/"),skip = 3,header = TRUE, sep = ",")
 
@@ -20,8 +18,19 @@ RH_Weekly_Mean <- aggregate(X. ~ cut(TimeST,"1 week"), RH, mean)
 colnames(RH_Weekly_Mean)[1] <- "WeeklyTime"
 RH_Weekly_Mean$WeeklyTimeST <- strptime(as.character(RH_Weekly_Mean$WeeklyTime),format = "%Y-%m-%d")
 # Plot
-jpeg("Test_output.jpeg",width = 1200,height = 600)
-plot(RH_Weekly_Mean$WeeklyTimeST,RH_Weekly_Mean$X.,pch=3,lty=1,xaxt="n", type = "o",xlab = "Date",ylab = "Relative Humidity")
-title("Relative Humidity")
+plot(RH_Weekly_Mean$WeeklyTimeST,RH_Weekly_Mean$X.,pch=3,lty=1,xaxt="n",yaxt="n", type = "o",xlab = "Date",ylab = "Relative Humidity", 
+     main = "Relative Humidity: Weekly Averaged")
 legend("topleft","top","Relative Humidity",pch=3,lty=1)
-dev.off()
+labelRH =  strftime(RH_Weekly_Mean$WeeklyTimeST,"%m/%d")
+axis(1, at=as.numeric(RH_Weekly_Mean$WeeklyTimeST), labels=labelRH, col.axis="black", las=2, cex.axis=0.7, tck=-0.02)
+i <- c(1:6)
+ypos <- (i-1)*20
+axis(2, at=ypos, col.axis="black", las=1, cex.axis=0.7, tck=-0.02)
+abline(h=c(50), lty=2, col="red")
+
+PRE$TimeST <- strptime(as.character(PRE$Time),format = "%Y/%m/%d %H:%M:%S")
+PRE_Weekly_Mean <- aggregate(Pascal ~ cut(TimeST,"1 week"), PRE, mean)
+colnames(PRE_Weekly_Mean)[1] <- "WeeklyTime"
+PRE_Weekly_Mean$WeeklyTimeST <- strptime(as.character(PRE_Weekly_Mean$WeeklyTime),format = "%Y-%m-%d")
+plot(PRE_Weekly_Mean$WeeklyTimeST,PRE_Weekly_Mean$Pascal,pch=3,lty=1,cex.lab=1, type = "o",xlab = "Date",ylab = "Relative Humidity", 
+     main = "Relative Humidity: Weekly Averaged",col="blue")
